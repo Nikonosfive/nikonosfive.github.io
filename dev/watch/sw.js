adc41cdfca16aa162986d6ffa4041655178b5a37
ニコワン <153420772+Nikonosfive@users.noreply.github.com>
2024-04-17 08:15:16 +0900

    Update sw.js

--- dev/watch/sw.js
+++ dev/watch/sw.js
@@ -1,1 +1,14 @@
 
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/6_00_0.html"
+    ]),
+  );
+});
